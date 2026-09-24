28b1fc8fcc5084bbe52ef8c2d8524d76405838f8
whwhsskjnk-dev <whwhsskjnk@gmail.com>
2026-09-24 19:04:56 +0300

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Replit Agent'
+description 'Use a medkit to restore your own health (QBCore)'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependency 'qb-core'
+dependency 'progressbar'
