dbab69639b7ee8e9b3c0d62c382c25686ab01d37
whwhsskjnk-dev <whwhsskjnk@gmail.com>
2026-09-24 19:04:51 +0300

    Add config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.ItemName = 'medkit'
+Config.ProgressDuration = 5000
+Config.UseCooldown = 2000
+Config.HealAmount = 100
+Config.MaxHealth = 200
+
+Config.ProgressLabel = 'Using medical kit...'
+Config.Messages = {
+    cannotUseWhileDown = 'You cannot use a medical kit while unconscious.',
+    alreadyHealthy = 'Your health is already full.',
+    missingItem = 'You do not have a medical kit.',
+    cancelled = 'Medical treatment cancelled.',
+    success = 'Your injuries have been treated.',
+    wait = 'Please wait before using another medical kit.',
+}
