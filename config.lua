ecb4813a95924abdd2754dd5517219548323bb6b
MAJEED <whwhsskjnk@gmail.com>
2026-09-25 11:07:19 +0300

    Fix medkit revive script: config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,26 @@
 Config = {}
 
 Config.ItemName = 'medkit'
+Config.TargetDistance = 5.0
 Config.ProgressDuration = 5000
 Config.UseCooldown = 2000
-Config.HealAmount = 100
-Config.MaxHealth = 200
+Config.ReviveEvent = 'hospital:client:Revive'
 
-Config.ProgressLabel = 'Using medical kit...'
+-- These match the CPR animation used by the current qb-ambulancejob resource.
+Config.Animation = {
+    dict = 'mini@cpr@char_a@cpr_str',
+    name = 'cpr_pumpchest',
+    flags = 33,
+}
+
+Config.ProgressLabel = 'Reviving player...'
 Config.Messages = {
     cannotUseWhileDown = 'You cannot use a medical kit while unconscious.',
-    alreadyHealthy = 'Your health is already full.',
+    noTarget = 'No player is close enough.',
+    targetNotDown = 'That player is not unconscious.',
     missingItem = 'You do not have a medical kit.',
-    cancelled = 'Medical treatment cancelled.',
-    success = 'Your injuries have been treated.',
+    cancelled = 'Revival cancelled.',
+    success = 'You revived the player.',
+    revived = 'You have been revived.',
     wait = 'Please wait before using another medical kit.',
 }
